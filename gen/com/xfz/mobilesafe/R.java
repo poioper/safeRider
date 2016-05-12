0adf3d416ab0e851a9d8cacee25b4bd259225a40
xiaofeng zhou <xfzhou1990@163.com>
2016-05-12 16:27:41 -0400

    interpolator and vibrator

--- gen/com/xfz/mobilesafe/R.java
+++ gen/com/xfz/mobilesafe/R.java
@@ -9,10 +9,12 @@ package com.xfz.mobilesafe;
 
 public final class R {
     public static final class anim {
-        public static final int tran_in=0x7f040000;
-        public static final int tran_out=0x7f040001;
-        public static final int tran_previous_in=0x7f040002;
-        public static final int tran_previous_out=0x7f040003;
+        public static final int cycle_7=0x7f040000;
+        public static final int shake=0x7f040001;
+        public static final int tran_in=0x7f040002;
+        public static final int tran_out=0x7f040003;
+        public static final int tran_previous_in=0x7f040004;
+        public static final int tran_previous_out=0x7f040005;
     }
     public static final class attr {
         /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
--- src/com/xfz/mobilesafe/activity/AddressActivity.java
+++ src/com/xfz/mobilesafe/activity/AddressActivity.java
@@ -5,10 +5,13 @@ import com.xfz.mobilesafe.db.dao.AddressDao;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -56,6 +59,24 @@ public class AddressActivity extends Activity {
 		if (!TextUtils.isEmpty(number)) {
 			String address = AddressDao.getAddress(number);
 			tvResult.setText(address);
+		} else {
+			Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+
+			// shake.setInterpolator(new Interpolator() {
+			//
+			// @Override
+			// public float getInterpolation(float x) {
+			// //y=ax+b
+			// int y=0;
+			// return y;
+			// }
+			// });
+			etNumber.startAnimation(shake);
+			vibrate();
 		}
+	}
+	private void vibrate() {
+		Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+		vibrator.vibrate(new long[] { 1000, 2000, 1000, 3000 }, -1);
 	}
 }
