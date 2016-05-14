38538f7f87052556ddabe121205c0d35bb24d964
xiaofeng zhou <xfzhou1990@163.com>
2016-05-13 20:23:58 -0400

    drag toast when phone ringing

--- src/com/xfz/mobilesafe/service/AddressService.java
+++ src/com/xfz/mobilesafe/service/AddressService.java
@@ -1,26 +1,25 @@
 package com.xfz.mobilesafe.service;
 
-import com.xfz.mobilesafe.R;
-import com.xfz.mobilesafe.db.dao.AddressDao;
-
-import android.R.integer;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.graphics.Color;
+import android.content.SharedPreferences.Editor;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.view.Gravity;
-import android.view.TextureView;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.xfz.mobilesafe.R;
+import com.xfz.mobilesafe.db.dao.AddressDao;
 
 /**
  * show incoming call service
@@ -36,6 +35,10 @@ public class AddressService extends Service {
 	private WindowManager mWM;
 	private View view;
 	private SharedPreferences mPref;
+	private int startX;
+	private int startY;
+	private int winWidth;
+	private int winHeight;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -67,6 +70,7 @@ public class AddressService extends Service {
 				// .show();
 				showToast(address);
 				break;
+				
 			case TelephonyManager.CALL_STATE_IDLE:
 				if (mWM != null && view != null) {
 					mWM.removeView(view);
@@ -98,24 +102,27 @@ public class AddressService extends Service {
 
 	private void showToast(String text) {
 		mWM = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+		
+		winWidth = mWM.getDefaultDisplay().getWidth();
+		winHeight = mWM.getDefaultDisplay().getHeight();
+		
 		final WindowManager.LayoutParams params = new WindowManager.LayoutParams();
 		;
 		params.height = WindowManager.LayoutParams.WRAP_CONTENT;
 		params.width = WindowManager.LayoutParams.WRAP_CONTENT;
 		params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-				| WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
 				| WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
 		params.format = PixelFormat.TRANSLUCENT;
-		params.type = WindowManager.LayoutParams.TYPE_TOAST;
+		params.type = WindowManager.LayoutParams.TYPE_PHONE;
 		params.gravity = Gravity.LEFT + Gravity.TOP;
 		params.setTitle("Toast");
-		
+
 		int lastX = mPref.getInt("lastX", 0);
 		int lastY = mPref.getInt("lastY", 0);
-		
+
 		params.x = lastX;
 		params.y = lastY;
-		
+
 		int style = mPref.getInt("address_style", 0);
 		int[] bgs = new int[] { R.drawable.call_locate_white,
 				R.drawable.call_locate_orange, R.drawable.call_locate_blue,
@@ -126,5 +133,60 @@ public class AddressService extends Service {
 		TextView tvText = (TextView) view.findViewById(R.id.tv_number);
 		tvText.setText(text);
 		mWM.addView(view, params);
+
+		view.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					startX = (int) event.getRawX();
+					startY = (int) event.getRawY();
+					break;
+				case MotionEvent.ACTION_MOVE:
+					int endX = (int) event.getRawX();
+					int endY = (int) event.getRawY();
+
+					int dx = endX - startX;
+					int dy = endY - startY;
+
+					// update window position
+					params.x += dx;
+					params.y += dy;
+					
+					if(params.x<0){
+						params.x = 0;
+					}
+					if(params.y<0){
+						params.y = 0;
+					}
+					if(params.x>winWidth-view.getWidth()){
+						params.x = winWidth-view.getWidth();
+					}
+					if(params.y>winHeight-view.getHeight()){
+						params.y = winHeight-view.getHeight();
+					}
+					
+					mWM.updateViewLayout(view, params);
+
+					startX = (int) event.getRawX();
+					startY = (int) event.getRawY();
+
+					break;
+				case MotionEvent.ACTION_UP:
+					Editor editor = mPref.edit();
+					editor.putInt("lastX", params.x);
+					editor.putInt("lastY", params.y);
+					editor.commit();
+					break;
+
+				default:
+					break;
+				}
+
+				return true;
+			}
+		});
 	}
+
 }
