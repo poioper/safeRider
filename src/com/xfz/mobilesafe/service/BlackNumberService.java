f3ad7242d1ee0453f7a4bdbe17d80b51d12f9a45
xiaofeng zhou <xfzhou1990@163.com>
2016-05-16 20:42:35 -0400

    using reflection to realize call interception

--- src/com/xfz/mobilesafe/service/BlackNumberService.java
+++ src/com/xfz/mobilesafe/service/BlackNumberService.java
@@ -1,6 +1,6 @@
 package com.xfz.mobilesafe.service;
 
-import com.xfz.mobilesafe.db.dao.BlackNumberDao;
+import java.lang.reflect.Method;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
@@ -8,17 +8,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 
-/** 
+import com.android.internal.telephony.ITelephony;
+import com.xfz.mobilesafe.db.dao.BlackNumberDao;
+
+/**
  * 
- * @author xfz:xfz1990@gmail.com 
- * @version create time：2016-5-16 
+ * @author xfz:xfz1990@gmail.com
+ * @version create time：2016-5-16
  */
 public class BlackNumberService extends Service {
 
 	private SmsReceiver receiver;
 	private BlackNumberDao blackNumberDao;
+	private TelephonyManager telephonyManager;
+	private MyPhoneStateListener myPhoneStateListener;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -30,20 +37,65 @@ public class BlackNumberService extends Service {
 		super.onCreate();
 		blackNumberDao = new BlackNumberDao(getApplicationContext());
 		receiver = new SmsReceiver();
-		//2.set listener
+		// 2.set listener
 		IntentFilter intentFilter = new IntentFilter();
 		intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
 		intentFilter.setPriority(1133);
-		//3.register broadcast receiver
+		// 3.register broadcast receiver
 		registerReceiver(receiver, intentFilter);
+		myPhoneStateListener = new MyPhoneStateListener();
+
+		telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+
+		telephonyManager.listen(myPhoneStateListener,
+				PhoneStateListener.LISTEN_CALL_STATE);
 	}
+
+	private class MyPhoneStateListener extends PhoneStateListener {
+		@Override
+		public void onCallStateChanged(int state, String incomingNumber) {
+			super.onCallStateChanged(state, incomingNumber);
+			if (state == TelephonyManager.CALL_STATE_RINGING) {
+				String mode = blackNumberDao.findNumber(incomingNumber);
+				if (mode.equals(BlackNumberDao.CALL)
+						|| mode.equals(BlackNumberDao.ALL)) {
+					endCall();
+				}
+			}
+		}
+	}
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		unregisterReceiver(receiver);
+		telephonyManager.listen(myPhoneStateListener,
+				PhoneStateListener.LISTEN_NONE);
 	}
-	
-	private class SmsReceiver extends BroadcastReceiver{
+
+	public void endCall() {
+		try {
+			// use reflection to realize servicemanager since it is hiden
+			Class<?> loadClass;
+
+			loadClass = Class.forName("android.os.ServiceManager");
+
+			Method method = loadClass.getDeclaredMethod("getService",
+					String.class);
+
+			IBinder invoke = (IBinder) method.invoke(null,
+					Context.TELEPHONY_SERVICE);
+
+			ITelephony iTelephony = ITelephony.Stub.asInterface(invoke);
+
+			iTelephony.endCall();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private class SmsReceiver extends BroadcastReceiver {
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
@@ -54,8 +106,9 @@ public class BlackNumberService extends Service {
 				String body = message.getMessageBody();
 				System.out.println(body + sender);
 				String mode = blackNumberDao.findNumber(sender);
-				if (mode.equals(BlackNumberDao.ALL) || mode.equals(BlackNumberDao.SMS)) {
-					//intercept sms message
+				if (mode.equals(BlackNumberDao.ALL)
+						|| mode.equals(BlackNumberDao.SMS)) {
+					// intercept sms message
 					abortBroadcast();
 				}
 			}
